8e5aa13e7a7169ffcc885e27a65a5d097f47d2a5
why10242018@outlook.com <why10242018@outlook.com>
2025-03-25 15:09:06 +0800

    向 ImGui 层添加了事件。

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -6,6 +6,9 @@
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
 
+#include "Hazel/ImGui/ImGuiLayer.h"
+
+
 // ---Entry Point---------------------
 #include "Hazel/EntryPoint.h"
 // -----------------------------------
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -27,6 +27,7 @@ public:
 	Sandbox()
     {
         PushLayer(new ExampleLayer());
+        PushOverlay(new Hazel::ImGuiLayer());
     }
 
 	~Sandbox() {}
