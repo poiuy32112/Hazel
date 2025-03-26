ddbb36d2251b6c7ede2675bef6840ef88d32ff32
why10242018@outlook.com <why10242018@outlook.com>
2025-03-26 13:40:49 +0800

    添加了输入轮询。

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Hazel
 {
 
@@ -60,6 +62,13 @@ namespace Hazel
 
             for (Layer* layer : m_LayerStack)
                 layer->OnUpdate();
+
+            auto[x, y] = Input::GetMousePosition();
+            HZ_CORE_TRACE("{0}, {1},", x, y);
+            //if (Input::IsKeyPressed(65))
+            //{
+            //    std::cout << "aaaaaaaaaaaaaaal\n";
+            //}
 
             m_Window->OnUpdate();
         }
