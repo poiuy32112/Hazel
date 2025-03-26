b1b392003410daaaf8fb89e36c072daa7fe8119a
why10242018@outlook.com <why10242018@outlook.com>
2025-03-26 18:35:34 +0800

    添加了 Hazel 键和鼠标代码。

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -63,8 +63,9 @@ namespace Hazel
             for (Layer* layer : m_LayerStack)
                 layer->OnUpdate();
 
-            auto[x, y] = Input::GetMousePosition();
-            HZ_CORE_TRACE("{0}, {1},", x, y);
+            //auto[x, y] = Input::GetMousePosition();
+            //HZ_CORE_TRACE("{0}, {1},", x, y);
+            
             //if (Input::IsKeyPressed(65))
             //{
             //    std::cout << "aaaaaaaaaaaaaaal\n";
