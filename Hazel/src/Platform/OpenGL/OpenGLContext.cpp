131911629249f47a63e26b3dfd2aa0096d6a9f6c
why10242018@outlook.com <why10242018@outlook.com>
2025-03-28 16:07:08 +0800

    我们的第一个三角形！

--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -19,6 +19,15 @@ namespace Hazel
         glfwMakeContextCurrent(m_WindowHandle);
         int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
         HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+        HZ_CORE_INFO("OpenGL Info:");
+        //HZ_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+        //HZ_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+        //HZ_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+        HZ_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+        HZ_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+        HZ_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
+
     }
 
     void OpenGLContext::SwapBuffers()
