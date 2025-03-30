889880ebb17534cf65427c6a651f1a74e0dc54d8
why10242018@outlook.com <why10242018@outlook.com>
2025-03-30 15:27:31 +0800

    添加了正交相机。

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -13,6 +13,8 @@
 #include "Hazel/Renderer/Buffer.h"
 #include "Hazel/Renderer/VertexArray.h"
 
+#include "Hazel/Renderer/OrthographicCamera.h"
+
 namespace Hazel
 {
 
@@ -45,6 +47,8 @@ namespace Hazel
 
         std::shared_ptr<Shader> m_BlueShader;
         std::shared_ptr<VertexArray> m_SquareVA;
+
+        OrthographicCamera m_Camera;
     private:
         static Application* s_Instance;
     };
