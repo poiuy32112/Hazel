a93250a36dee3943e540a2e93731a28f91763b14
why10242018@outlook.com <why10242018@outlook.com>
2025-03-30 12:36:20 +0800

    添加了 RendererAPI 和 RenderCommand 架构。

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -3,6 +3,18 @@
 
 namespace Hazel
 {
+    void Renderer::BeginScene()
+    {
+    }
 
-    RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+    void Renderer::EndScene()
+    {
+    }
+
+    void Renderer::Submit(const std::shared_ptr<VertexArray>& vertexArray)
+    {
+        vertexArray->Bind();
+        RenderCommand::DrawIndexed(vertexArray);
+    }
+
 }
