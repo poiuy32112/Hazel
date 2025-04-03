21fd4deeb660fd5ca49fd0f9d3545674cd4420de
why10242018@outlook.com <why10242018@outlook.com>
2025-04-03 18:20:27 +0800

    添加了适当的窗口大小调整处理

--- Hazel/src/Hazel/Renderer/Renderer.h
+++ Hazel/src/Hazel/Renderer/Renderer.h
@@ -13,6 +13,10 @@ namespace Hazel
     public:
         static void Init();
 
+        //-
+        static void OnWindowResize(uint32_t width, uint32_t height);
+        //-
+
         static void BeginScene(OrthographicCamera& camera);
         static void EndScene();
 
