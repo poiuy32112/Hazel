dec80395ab97fef4e59d1516bf06c3502a35bdce
why10242018@outlook.com <why10242018@outlook.com>
2025-04-07 10:33:03 +0800

    向 Renderer2D 添加了基本批处理

--- Hazel/src/Hazel/Renderer/RenderCommand.h
+++ Hazel/src/Hazel/Renderer/RenderCommand.h
@@ -30,9 +30,9 @@ namespace Hazel
             s_RendererAPI->Clear();
         }
 
-        inline static void DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray)
+        inline static void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t count = 0)
         {
-            s_RendererAPI->DrawIndexed(vertexArray);
+            s_RendererAPI->DrawIndexed(vertexArray, count);
         }
     private:
         static RendererAPI* s_RendererAPI;
