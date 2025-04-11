f0c5e4c482651a5883aad2dbd11c4da5ad167424
why10242018@outlook.com <why10242018@outlook.com>
2025-04-11 20:38:02 +0800

    添加了 Entity 类和 TagComponent

--- Hazel/src/Hazel/Scene/Scene.cpp
+++ Hazel/src/Hazel/Scene/Scene.cpp
@@ -6,6 +6,8 @@
 
 #include <glm/glm.hpp>
 
+#include "Entity.h"
+
 namespace Hazel
 {
 
@@ -50,9 +52,13 @@ namespace Hazel
     {
     }
 
-    entt::entity Scene::CreateEntity()
+    Entity Scene::CreateEntity(const std::string& name)
     {
-        return m_Registry.create();
+        Entity entity = { m_Registry.create(), this };
+        entity.AddComponent<TransformComponent>();
+        auto& tag = entity.AddComponent<TagComponent>();
+        tag.Tag = name.empty() ? "Entity" : name;
+        return entity;
     }
 
     void Scene::OnUpdate(Timestep ts)
