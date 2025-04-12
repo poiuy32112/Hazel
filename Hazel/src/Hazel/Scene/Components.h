791a1c1e7563a2ed58c4d2d5d83b54f2c3192660
why10242018@outlook.com <why10242018@outlook.com>
2025-04-12 15:07:56 +0800

    添加了 SceneCamera + 摄像机大小调整支持

--- Hazel/src/Hazel/Scene/Components.h
+++ Hazel/src/Hazel/Scene/Components.h
@@ -2,7 +2,7 @@
 
 #include <glm/glm.hpp>
 
-#include "Hazel/Renderer/Camera.h"
+#include "SceneCamera.h"
 
 namespace Hazel
 {
@@ -47,15 +47,12 @@ namespace Hazel
 
     struct CameraComponent
     {
-        Hazel::Camera Camera;
+        SceneCamera Camera;
         bool Primary = true; // TODO: think about moving to Scene
+        bool FixedAspectRatio = false;
 
         CameraComponent() = default;
         CameraComponent(const CameraComponent&) = default;
-        CameraComponent(const glm::mat4& projection)
-            : Camera(projection)
-        {
-        }
     };
 
 }
