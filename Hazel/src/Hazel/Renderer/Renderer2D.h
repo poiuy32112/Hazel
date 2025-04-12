0b60ce3038dc876f1f7a52df3a8a1d72a8059728
why10242018@outlook.com <why10242018@outlook.com>
2025-04-12 11:56:51 +0800

    添加了 camera 组件并将 2D 渲染移动到 Scene 类

--- Hazel/src/Hazel/Renderer/Renderer2D.h
+++ Hazel/src/Hazel/Renderer/Renderer2D.h
@@ -4,6 +4,8 @@
 
 #include "Texture.h"
 
+#include "Hazel/Renderer/Camera.h"
+
 namespace Hazel
 {
 
@@ -13,7 +15,8 @@ namespace Hazel
         static void Init();
         static void Shutdown();
 
-        static void BeginScene(const OrthographicCamera& camera);
+        static void BeginScene(const Camera& camera, const glm::mat4& transform);
+        static void BeginScene(const OrthographicCamera& camera); // TODO: Remove
         static void EndScene();
 
         static void Flush();
--- Hazel/src/Hazel/Scene/Components.h
+++ Hazel/src/Hazel/Scene/Components.h
@@ -2,6 +2,8 @@
 
 #include <glm/glm.hpp>
 
+#include "Hazel/Renderer/Camera.h"
+
 namespace Hazel
 {
     struct TagComponent
@@ -39,6 +41,19 @@ namespace Hazel
         SpriteRendererComponent(const SpriteRendererComponent&) = default;
         SpriteRendererComponent(const glm::vec4& color)
             : Color(color)
+        {
+        }
+    };
+
+    struct CameraComponent
+    {
+        Hazel::Camera Camera;
+        bool Primary = true; // TODO: think about moving to Scene
+
+        CameraComponent() = default;
+        CameraComponent(const CameraComponent&) = default;
+        CameraComponent(const glm::mat4& projection)
+            : Camera(projection)
         {
         }
     };
