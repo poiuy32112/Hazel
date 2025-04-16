093deffb51254a03e803a3f3dbbded57aeb17389
why10242018@outlook.com <why10242018@outlook.com>
2025-04-16 17:16:49 +0800

    向 Hazelnut + Win32 打开/保存文件对话框添加了新建、打开和另存为文件菜单选项

--- Hazelnut/src/EditorLayer.h
+++ Hazelnut/src/EditorLayer.h
@@ -19,6 +19,12 @@ namespace Hazel
         virtual void OnImGuiRender() override;
         void OnEvent(Event& e) override;
     private:
+        bool OnKeyPressed(KeyPressedEvent& e);
+
+        void NewScene();
+        void OpenScene();
+        void SaveSceneAs();
+    private:
         Hazel::OrthographicCameraController m_CameraController;
 
         // Temp
